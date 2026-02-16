e20cb35a290b1c1eb8a8567e9d8b75003104b871
Tanmay Garg <tanmaygarg9879@gmail.com>
2026-02-16 16:13:31 +0100

    Add exclude argument to `get_total_size` function to exclude specific files.

--- src/qlever/util.py
+++ src/qlever/util.py
@@ -19,17 +19,22 @@ import psutil
 from qlever.log import log
 
 
-def get_total_file_size(patterns: list[str]) -> int:
+def get_total_file_size(
+    patterns: list[str], exclude: set[str] | None = None
+) -> int:
     """
-    Helper function that gets the total size of all files mathing the given
-    patterns in bytes.
+    Helper function that gets the total size of all files matching the given
+    patterns in bytes. Files whose names match any entry in `exclude` are
+    skipped.
     """
-
+    if not exclude:
+        exclude = set()
     total_size = 0
     search_dir = Path.cwd()
     for pattern in patterns:
         for file in search_dir.glob(pattern):
-            total_size += file.stat().st_size
+            if file.name not in exclude:
+                total_size += file.stat().st_size
     return total_size
 
 
